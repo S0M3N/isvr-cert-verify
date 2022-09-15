5c5a8abad4584c413c00153458eaa7e9159cf2bd
S0M3N <thesomen123@gmail.com>
2022-09-15 14:57:08 +0530

    final host commit

--- cvs/settings.py
+++ cvs/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 #     BASE_DIR / "static",
 # ]
 
-STATIC_ROOT = os.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 
 # Default primary key field type
